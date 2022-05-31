381b9f430b0971f97f6904e0c0b2805bd21c5d80
lucas-vrm <luccas_menezzes@hotmail.com>
2022-05-31 11:33:03 -0300

    criada uma view para a action atual

--- benben/benben WatchKit Extension/View/GameView.swift
+++ benben/benben WatchKit Extension/View/GameView.swift
@@ -9,25 +9,13 @@ import SwiftUI
 
 struct GameView: View {
     @ObservedObject var gameViewModel: GameViewModel = GameViewModel()
-    @State var actionText: String = ""
     
     var body: some View {
         switch gameViewModel.state {
         case .HOME:
             StartGameView(highScore: 30, gameViewModel: gameViewModel)
         case .PLAYING:
-            ZStack{
-                Circle()
-                    .stroke(Color.gray.opacity(0.2), style: StrokeStyle(lineWidth: 15, lineCap: .round))
-                Circle()
-                    //.trim(from: 0, to: 1 - ((defaultTimeRemaining  - timeRemaining)/defaultTimeRemaining))
-                    .stroke(Color.red, style: StrokeStyle(lineWidth: 15, lineCap: .round))
-                    .rotationEffect(.degrees(-90))
-                    //.animation(.easeInOut(duration: 1.5), value: timeRemaining)
-                Text(actionText).onReceive(gameViewModel.$currentAction) { action in
-                    actionText = action!.text
-                }             
-            }
+            ActionView(gameViewModel: gameViewModel)
         case .RIGHTACTION:
             FeedbackActionView(actionFeedback: .right)
         case .WRONGACTION:
